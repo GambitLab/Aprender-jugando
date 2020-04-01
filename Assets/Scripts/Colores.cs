c0bb4bee69d253aabcd25add2625235604bca5a5
gantusnicolas <gantus.nicolas@gmail.com>
2020-03-31 21:32:50 -0300

    Assets actualizados

---
+++ Assets/Scripts/Colores.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using UnityEngine.Events;
+
+public class Colores : MonoBehaviour
+{
+    public KeywordRecognizer keywordRecognizer;
+    public string[] palabras;
+    public Color[] colores;
+    private string palabraActual;
+    private int indiceAnterior = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CambiarColor();
+        
+        keywordRecognizer = new KeywordRecognizer(palabras);
+        keywordRecognizer.OnPhraseRecognized += reconocer;
+        keywordRecognizer.Start();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void reconocer(PhraseRecognizedEventArgs escuchado)
+    {
+        string palabra = escuchado.text;
+        if(palabra == palabraActual)
+        {
+            //Le pego
+            CambiarColor();
+        }
+        else
+        {
+            //No le pego
+
+        }
+    }
+
+    public void CambiarColor()
+    {
+        int indice;
+        do {
+            indice = Random.Range(0, palabras.Length);
+        } while (indiceAnterior == indice);
+
+        indiceAnterior = indice;
+
+        palabraActual = palabras[indice];
+        Camera.main.backgroundColor = colores[indice];
+    }
+}
---
+++ Assets/Scripts/ReconocimientoVoz.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public abstract class ReconocimientoVoz : MonoBehaviour
+{
+
+    
+    public Dictionary<string, UnityAction> diccionario = new Dictionary<string, UnityAction>();
+
+    public ReconocimientoVoz(string[] keywords)
+    {
+        
+    }
+
+    
+}
